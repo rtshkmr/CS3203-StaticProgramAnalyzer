2d9ea374f99a76257cefba56fcdda60b7f46e77d
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-08-31 23:52:40 +0800

    Update cMakeLists.txt

---
+++ Team00/Code00/src/spa/src/component/QueryProcessor/QueryEvaluator.h
@@ -0,0 +1,11 @@
+#ifndef INC_21S1_CP_SPA_TEAM_35_QUERYEVALUATOR_H
+#define INC_21S1_CP_SPA_TEAM_35_QUERYEVALUATOR_H
+
+#include <list>
+
+class QueryEvaluator {
+    public:
+        // evaluateQuery(std::list<Synonym> syn_list, Synonym target, std::list<Group> groups);
+};
+
+#endif //INC_21S1_CP_SPA_TEAM_35_QUERYEVALUATOR_H
---
+++ Team00/Code00/src/spa/src/component/QueryProcessor/QueryProjector.h
@@ -0,0 +1,13 @@
+#ifndef INC_21S1_CP_SPA_TEAM_35_QUERYPROJECTOR_H
+#define INC_21S1_CP_SPA_TEAM_35_QUERYPROJECTOR_H
+
+#include "model/Statement.h"
+//#include "component/model/Variable.h"
+
+class QueryProjector {
+    public:
+        // formatQuery(std::list<Statement> statement_list);
+        // formatQuery(std::list<Variable> variable_list);
+};
+
+#endif //INC_21S1_CP_SPA_TEAM_35_QUERYPROJECTOR_H
