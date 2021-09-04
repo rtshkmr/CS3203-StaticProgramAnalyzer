0a6ca86b0c4f3bf15b5ab5adfd1d556d9b3c12fb
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-09-04 16:01:08 +0800

    Add skeleton for QueryExtractor

--- Team00/Code00/src/autotester/src/TestWrapper.cpp
+++ Team00/Code00/src/autotester/src/TestWrapper.cpp
@@ -1,4 +1,5 @@
 #include "TestWrapper.h"
+#include <component/QueryProcessor/QueryExtractor/QueryExtractor.h>
 #include "component/SourceProcessor/SourceProcessor.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
@@ -25,7 +26,11 @@ void TestWrapper::parse(std::string file_name) {
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 // call your evaluator to evaluate the query here
+  // ...code to extract query...
+  auto query_extractor = new QueryExtractor(query);
+  query_extractor->extractQuery();
   // ...code to evaluate query...
+  // e.g. evaluateQuery(query_extractor->getSynonymList(), query_extractor->getTarget(), query_extractor->getGroups());
 
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
---
+++ Team00/Code00/src/spa/src/component/QueryProcessor/QueryExtractor/QueryExtractor.cpp
@@ -0,0 +1,10 @@
+#include "QueryExtractor.h"
+
+#include <iostream>
+
+void QueryExtractor::extractQuery() {
+  /*
+   * tokenize, parse, validation logic goes here. Can call helper classes to achieve the aforementioned.
+   * store required values in attributes of QueryExtractor instance.
+   */
+}
