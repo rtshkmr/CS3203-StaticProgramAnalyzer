15ed51146cb343c46ea605bef005d30fb7dec2f0
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-09-06 16:44:01 +0800

    Resolve dependencies

--- Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.cpp
+++ Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.cpp
@@ -1,7 +1,9 @@
 #include "Types.h"
 
 bool Group::addClauseToVector(Clause clause) {
+  auto initialSize = clauses.size();
   clauses.push_back(clause);
+  return (clauses.size() == initialSize + 1);
 }
 
 std::vector<Clause> Group::getClauses() {
