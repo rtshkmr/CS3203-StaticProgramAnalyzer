185c0024cc30744c46eddb47f314166f7b4f2c30
maxxng <maxngkaishen@gmail.com>
2021-09-06 16:10:24 +0800

    Add Datastructure for QueryExtractor and Evaluator API

--- Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.h
+++ Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.h
@@ -16,6 +16,23 @@ enum class DesignEntity {
   kProcedure
 };
 
+enum class RelRef {
+  kModifiesP,
+  kModifiesS,
+  kUsesP,
+  kUsesS,
+  kCalls,
+  kCallsT,
+  kParent,
+  kParentT,
+  kFollows,
+  kFollowsT,
+  kNext,
+  kNextT,
+  kAffects,
+  kAffectsT,
+};
+
 class Synonym {
   private:
     std::string name;
@@ -25,8 +42,41 @@ class Synonym {
     DesignEntity getType() { return type; };
 };
 
+struct Clause {
+  ClauseType ct;
+  std::string leftHandSide;
+  std::string rightHandSide;
+
+};
+
+class Clause {
+  public:
+  std::string leftHandSide;
+  std::string rightHandSide;
+};
+
+class SuchThat : Clause {
+  public:
+    RelRef relRef;
+    bool leftIsSynonym;
+    bool rightIsSynonym;
+};
+
+class Pattern : Clause {
+  public:
+    std::string assignSynonym;
+    bool leftIsSynonym;
+    bool isExact;
+};
+
 class Group {
-  // TODO
+public:
+  bool addClauseToVector(Clause clause);
+  std::vector<Clause> getClauses();
+  bool containsTargetSynonym();
+private:
+  std::vector<Clause> clauses;
+  bool hasTargetSynonym;
 };
 
 #endif //AUTOTESTER_TYPES_H
