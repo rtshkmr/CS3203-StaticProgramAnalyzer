607b2d8b2ca00818c6e8c67516095e3a4bea5470
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-09-06 16:25:31 +0800

    add implementation for Groups class

--- Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.cpp
+++ Team00/Code00/src/spa/src/component/QueryProcessor/types/Types.cpp
@@ -1,1 +1,13 @@
 #include "Types.h"
+
+bool Group::addClauseToVector(Clause clause) {
+  clauses.push_back(clause);
+}
+
+std::vector<Clause> Group::getClauses() {
+  return clauses;
+}
+
+bool Group::containsTargetSynonym() {
+  return hasTargetSynonym;
+}
