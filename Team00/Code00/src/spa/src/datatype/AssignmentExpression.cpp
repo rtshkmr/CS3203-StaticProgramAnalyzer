3c905779db78065e4e42a5ac379d62c3b5e2a3fc
Tan Wei Jie <tanweijie123@gmail.com>
2021-09-10 17:31:46 +0800

    AssignmentExpression infix to postfix first version

--- Team00/Code00/src/spa/src/datatype/AssignmentExpression.cpp
+++ Team00/Code00/src/spa/src/datatype/AssignmentExpression.cpp
@@ -2,6 +2,8 @@
  * This body file contains the implementation of AssignmentExpression functions.
  */
 
+#include <regex>
+#include <stack>
 #include "AssignmentExpression.h"
 
 /**
@@ -15,17 +17,145 @@ AssignmentExpression::AssignmentExpression(std::string infix) {
 }
 
 /**
+ * Check if the given character is an operator.
+ * An operator character refers to ( ) + - * / % ^
+ * @param c The character to check
+ * @return true if it is one of the character; false otherwise.
+ */
+bool IsOperator(char &c) {
+  if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^' || c == '(' || c == ')')
+    return true;
+  else
+    return false;
+}
+
+// Operator Rank only returns the input value rank.
+// lower rank = push
+// always return an odd number so stack value is even.
+int OperatorRank(char &c) {
+  if (c == ')') return 7;
+  if (c == '(') return -1;
+  if (c == '+' || c == '-') return 1;
+  if (c == '*' || c == '/' || c == '%') return 3;
+  if (c == '^') return 5;
+}
+
+std::string ConvertToStringAndClearBuffer(std::vector<char>* chara) {
+  std::string s(chara->begin(), chara->end());
+  chara->clear();
+
+  if (!s.empty()) {
+    if (s.back() != ' ') {
+      s.push_back(' '); /// IMPORTANT: delimit variable 'a' and 'b' with 'ab'
+    }
+  }
+
+  return s;
+}
+
+std::vector<std::string> ConvertEquationIntoTokens(std::string eqn) {
+  std::vector<char> chara;
+  std::vector<std::string> tokens;
+
+  std::stack<char> operator_stack;
+
+  for (char &c: eqn) {
+    if (IsOperator(c)) {
+      std::string s = ConvertToStringAndClearBuffer(&chara);
+      if (!s.empty()) {
+        tokens.push_back(s);
+      }
+
+      int rank = OperatorRank(c);
+      if (rank == -1) {
+        operator_stack.push(c);
+        continue;
+      }
+      if (!operator_stack.empty()) {
+        int stack_rank = OperatorRank(operator_stack.top()) + 1; //stack rank always higher, due to left-associativity
+
+        if (rank == 7) { //close bracket --> force pop to stack's (
+          int op_rank = 0;
+          do {
+            char op = operator_stack.top();
+            operator_stack.pop();
+            std::string s2;
+            s2.push_back(op);
+
+            op_rank = OperatorRank(op);
+            if (op_rank != -1) {
+              tokens.push_back(s2);
+            } else {
+              break;
+            }
+          } while (op_rank != -1);
+
+        } else if (rank < stack_rank) {
+          while (rank < stack_rank) {
+            char op = operator_stack.top();
+            operator_stack.pop();
+            std::string s2;
+            s2.push_back(op);
+            tokens.push_back(s2);
+
+            if (!operator_stack.empty())
+              stack_rank = OperatorRank(operator_stack.top()) + 1;
+            else break;
+          }
+          operator_stack.push(c);
+        } else {
+          operator_stack.push(c);
+        }
+      } else {
+        operator_stack.push(c);
+      }
+    } else if (c != ' ') {
+      chara.push_back(c);
+    }
+  }
+
+  std::string s = ConvertToStringAndClearBuffer(&chara);
+  if (!s.empty()) {
+    tokens.push_back(s);
+  }
+
+  while (!operator_stack.empty()) {
+    char op = operator_stack.top();
+    operator_stack.pop();
+    std::string s2;
+    s2.push_back(op);
+    tokens.push_back(s2);
+  }
+
+  return tokens;
+}
+
+/**
  * This function contains the algorithm to convert an infix assignment expression into postfix expression.
  * @param infix The infix assignment to be converted.
  * @return The assignment which is transformed into postfix style.
  */
 std::string AssignmentExpression::ParseInfixToPostfix(std::string infix) {
-  //TODO: change infix into postfix
-  return infix;
+
+  ///Assumption: equation has been checked, and in correct format.
+
+  std::vector<std::string> tokens = ConvertEquationIntoTokens(infix);
+
+  std::string combine;
+
+  for (std::string s: tokens) {
+    combine.append(s);
+  }
+
+  return combine;
 }
 
 std::string AssignmentExpression::GetExpressionString() {
-  return expression_;
+  if (expression_.back() == ' ') {
+    return expression_.substr(0, expression_.size() - 1);
+  } else {
+    return expression_;
+  }
 }
 
 /**
@@ -37,9 +167,11 @@ std::string AssignmentExpression::GetExpressionString() {
 bool AssignmentExpression::CheckExist(std::string pattern) {
   std::string queryPostfix = ParseInfixToPostfix(pattern);
 
-  //TODO: check if expression CONTAINS queryPostfix
-
-  return true;
+  if (expression_.find(queryPostfix) != std::string::npos) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -51,35 +183,5 @@ bool AssignmentExpression::CheckExist(std::string pattern) {
 bool AssignmentExpression::CheckExact(std::string pattern) {
   std::string queryPostfix = ParseInfixToPostfix(pattern);
 
-  //TODO: check if expression == queryPostfix
-
-  return true;
-}
-
-/**
- * This function will check if the expression is ends with the pattern given.
- * This is used in condition of _"infix"
- * @param pattern The pattern to evaluate against this assignment expression.
- * @return true if exist; false otherwise.
- */
-bool AssignmentExpression::CheckBefore(std::string pattern) {
-  std::string queryPostfix = ParseInfixToPostfix(pattern);
-
-  //TODO: check if expression ENDS WITH pattern
-
-  return true;
-}
-
-/**
- * This function will check if the expression is starts with the pattern given.
- * This is used in condition of "infix"_
- * @param pattern The pattern to evaluate against this assignment expression.
- * @return true if exist; false otherwise.
- */
-bool AssignmentExpression::CheckAfter(std::string pattern) {
-  std::string queryPostfix = ParseInfixToPostfix(pattern);
-
-  //TODO: check if expression STARTS WITH pattern
-
-  return true;
+  return expression_ == queryPostfix;
 }
