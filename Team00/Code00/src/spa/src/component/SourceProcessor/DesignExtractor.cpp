3e5023b740a6f3791e70a23b495f7c61343854fa
LimJunxue <junxue1@hotmail.com>
2021-09-10 18:35:31 +0800

    Added skeleton for DE and transitive relationship maps
    Also changed the return type of GetDeliverables in Parser, and renamed maps relevant to parent to follow a key_to_value_map structure.

--- Team00/Code00/src/spa/src/component/SourceProcessor/DesignExtractor.cpp
+++ Team00/Code00/src/spa/src/component/SourceProcessor/DesignExtractor.cpp
@@ -1,4 +1,49 @@
 #include "DesignExtractor.h"
 
-void de::DesignExtractor::ExtractDesignAbstractions() {
+/**
+ * Extracts transitive relationships using the lists and tables from the deliverable.
+ */
+void DesignExtractor::ExtractDesignAbstractions(Deliverable* deliverable) {
+  this->deliverable_ = deliverable;
+
+  ExtractUses();
+  ExtractModifies();
+  ExtractParentT(deliverable->parent_to_child_hash_);
+  ExtractFollowsT(deliverable->follow_hash_);
+  ExtractParentOfT(deliverable->child_to_parent_hash_);
+  ExtractFollowedByT(deliverable->followed_by_hash_);
+}
+
+/**
+ * Extracts transitive Uses relationship from nested containers and called procedures.
+ * For e.g. there can be a while container contained in another while container.
+ * A procedure can also call another procedure.
+ */
+void DesignExtractor::ExtractUses() {
+
+}
+
+/**
+ * Extracts transitive Modifies relationship from nested containers and called procedures.
+ * For e.g. there can be a while container contained in another while container.
+ * A procedure can also call another procedure.
+ */
+void DesignExtractor::ExtractModifies() {
+
+}
+
+void DesignExtractor::ExtractParentT(std::unordered_map<Statement*, std::list<Statement*>*> parent_hash) {
+
+}
+
+void DesignExtractor::ExtractFollowsT(std::unordered_map<Statement*, Statement*> follow_hash) {
+
+}
+
+void DesignExtractor::ExtractParentOfT(std::unordered_map<Statement*, Statement*> child_to_parent_hash) {
+
+}
+
+void DesignExtractor::ExtractFollowedByT(std::unordered_map<Statement*, Statement*> followed_by_hash) {
+
 }
--- Team00/Code00/src/spa/src/component/SourceProcessor/DesignExtractor.h
+++ Team00/Code00/src/spa/src/component/SourceProcessor/DesignExtractor.h
@@ -1,22 +1,25 @@
 #ifndef AUTOTESTER_DESIGNEXTRACTOR_H
 #define AUTOTESTER_DESIGNEXTRACTOR_H
 
-/**
- * This namespace provides a scope for DesignExtractor related declarations.
- */
-namespace de {
-    /**
-     * This class encapsulates the extraction of design relationships from the data structures available in the Source
-     * Processor.
-     */
-    class DesignExtractor {
-    private:
-        // attributes
-    public:
-        DesignExtractor() = default;
+#include <datatype/Deliverable.h>
 
-        void ExtractDesignAbstractions();
-    };
-}
+/**
+ * This class encapsulates the extraction of design relationships from the data structures available in the Source
+ * Processor.
+ */
+class DesignExtractor {
+ public:
+  DesignExtractor() = default;
+  void ExtractDesignAbstractions(Deliverable* deliverable);
+ private:
+  Deliverable* deliverable_;
+
+  void ExtractUses(); // todo: add arguments when needed
+  void ExtractModifies(); // todo: add arguments when needed
+  void ExtractParentT(std::unordered_map<Statement*, std::list<Statement*>*> parent_hash);
+  void ExtractFollowsT(std::unordered_map<Statement*, Statement*> follow_hash);
+  void ExtractParentOfT(std::unordered_map<Statement*, Statement*> child_to_parent_hash);
+  void ExtractFollowedByT(std::unordered_map<Statement*, Statement*> followed_by_hash);
+};
 
 #endif //AUTOTESTER_DESIGNEXTRACTOR_H
