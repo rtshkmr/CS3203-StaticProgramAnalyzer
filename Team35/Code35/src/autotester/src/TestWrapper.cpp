d18ee799320a5c89e337caad2527f00a54b3a316
WeiJie <tanweijie123@gmail.com>
2021-09-15 22:05:29 +0800

    Update TestWrapper.cpp

--- Team35/Code35/src/autotester/src/TestWrapper.cpp
+++ Team35/Code35/src/autotester/src/TestWrapper.cpp
@@ -1,6 +1,7 @@
 #include "TestWrapper.h"
 #include "component/SourceProcessor/SourceProcessor.h"
 #include <component/QueryProcessor/QuerySystemController.h>
+#include <sstream>
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -27,12 +28,26 @@ void TestWrapper::parse(std::string file_name) {
     sp::SourceProcessor::ProcessSourceFile(file_name, pkb);
 }
 
+std::vector<std::string> split(std::string const &input) {
+  std::istringstream buffer(input);
+  std::vector<std::string> ret;
+
+  std::copy(std::istream_iterator<std::string>(buffer),
+            std::istream_iterator<std::string>(),
+            std::back_inserter(ret));
+  return ret;
+}
+
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 // call your evaluator to evaluate the query here
   std::cout << "Point 3 in test wrapper" << std::endl;
   std::optional<std::string> s = QuerySystemController::Evaluate(&query, pkb);
   if (s) {
-    results.push_back(s.value());;
+    std::vector<std::string> res_vec = split(s.value());
+
+    for (std::string s : res_vec) {
+      results.push_back(s);
+    }
   }
 }
