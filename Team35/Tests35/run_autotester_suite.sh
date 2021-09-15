0b6819de816a7ec18168e0b8186c8040044496a0
Tan Wei Jie <tanweijie123@gmail.com>
2021-09-15 13:11:30 +0800

    Update run_autotester_suite.sh
    Missing extension for textfile, making it invalid path to run

--- Team35/Tests35/run_autotester_suite.sh
+++ Team35/Tests35/run_autotester_suite.sh
@@ -10,7 +10,7 @@ END=$(ls test_source_*.txt | wc -l);
 echo "[NOTE] Running a grand total of $END tests..."; 
 
 for i in $(seq 1 $END);
-do echo "[NOTE] Running Test $i"; ./../autotester test_source_$i.txt test_queries_$i output_$i.xml;
+do echo "[NOTE] Running Test $i"; ./../autotester test_source_$i.txt test_queries_$i.txt output_$i.xml;
 done
 
 zip output_final . -i 'output_*.xml';
