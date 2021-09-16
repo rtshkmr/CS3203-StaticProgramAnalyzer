b60b73629d73e71af2a254993c934f69a092de12
maxxng <maxngkaishen@gmail.com>
2021-09-16 16:19:40 +0800

    Remove sort method and only retain unique method

--- Team35/Code35/src/spa/src/component/QueryProcessor/QueryProjector.cpp
+++ Team35/Code35/src/spa/src/component/QueryProcessor/QueryProjector.cpp
@@ -3,8 +3,8 @@
 std::string QueryProjector::FormatQuery(std::vector<std::string> result_list) {
   std::string output;
 
-  std::sort( result_list.begin(), result_list.end() );
-  result_list.erase( unique( result_list.begin(), result_list.end() ), result_list.end() );
+
+  result_list.erase( std::unique( result_list.begin(), result_list.end() ), result_list.end() );
 
   for (std::string result : result_list) {
     output += result;
