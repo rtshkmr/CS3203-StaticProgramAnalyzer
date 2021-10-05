1695d96e223660f58d2cedec7849d791abd89bb5
LimJunxue <junxue1@hotmail.com>
2021-10-05 18:46:48 +0800

    Add Calls* extraction
    With positive and negative unit test all

--- Team35/Code35/src/spa/src/component/SourceProcessor/Extractors/CallsTExtractor.h
+++ Team35/Code35/src/spa/src/component/SourceProcessor/Extractors/CallsTExtractor.h
@@ -9,6 +9,7 @@ class CallsTExtractor {
   void Extract(Deliverable* deliverable);
  private:
   Deliverable* deliverable_;
+  std::list<Procedure*>* ExtractCallsTFromProc(Procedure* calling_proc, std::vector<Procedure*>* visited_procedures);
 };
 
 #endif //AUTOTESTER_CODE35_SRC_SPA_SRC_COMPONENT_SOURCEPROCESSOR_EXTRACTORS_CALLSTEXTRACTOR_H_
