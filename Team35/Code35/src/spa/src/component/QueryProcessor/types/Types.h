f9e03fc3955eea20ac59f5cf9a13f4ab59bffa6a
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-10-05 16:18:27 +0800

    Transfer enum getters from types header to source

--- Team35/Code35/src/spa/src/component/QueryProcessor/types/Types.h
+++ Team35/Code35/src/spa/src/component/QueryProcessor/types/Types.h
@@ -20,31 +20,7 @@ enum class DesignEntity {
   kInvalid
 };
 
-static DesignEntity GetDesignEntity(std::string reference) {
-  if (reference == "stmt") {
-    return DesignEntity::kStmt;
-  } else if (reference == "read") {
-    return DesignEntity::kRead;
-  } else if (reference == "print") {
-    return DesignEntity::kPrint;
-  } else if (reference == "call") {
-    return DesignEntity::kCall;
-  } else if (reference == "while") {
-    return DesignEntity::kWhile;
-  } else if (reference == "if") {
-    return DesignEntity::kIf;
-  } else if (reference == "assign") {
-    return DesignEntity::kAssign;
-  } else if (reference == "variable") {
-    return DesignEntity::kVariable;
-  } else if (reference == "constant") {
-    return DesignEntity::kConstant;
-  } else if (reference == "procedure") {
-    return DesignEntity::kProcedure;
-  }
-
-  return DesignEntity::kInvalid;
-}
+DesignEntity GetDesignEntity(std::string reference);
 
 enum class RelRef {
   kModifiesP,
@@ -64,38 +40,7 @@ enum class RelRef {
   kInvalid
 };
 
-static RelRef GetRelRef(std::string reference) {
-  if (reference == "ModifiesP") {
-    return RelRef::kModifiesP;
-  } else if (reference == "ModifiesS") {
-    return RelRef::kModifiesS;
-  } else if (reference == "UsesP") {
-    return RelRef::kUsesP;
-  } else if (reference == "UsesS") {
-    return RelRef::kUsesS;
-  } else if (reference == "Calls") {
-    return RelRef::kCalls;
-  } else if (reference == "Calls*") {
-    return RelRef::kCallsT;
-  } else if (reference == "Parent") {
-    return RelRef::kParent;
-  } else if (reference == "Parent*") {
-    return RelRef::kParentT;
-  } else if (reference == "Follows") {
-    return RelRef::kFollows;
-  } else if (reference == "Follows*") {
-    return RelRef::kFollowsT;
-  } else if (reference == "Next") {
-    return RelRef::kNext;
-  } else if (reference == "Next*") {
-    return RelRef::kNextT;
-  } else if (reference == "Affects") {
-    return RelRef::kAffects;
-  } else if (reference == "Affects*") {
-    return RelRef::kAffectsT;
-  }
-  return RelRef::kInvalid;
-}
+RelRef GetRelRef(std::string reference);
 
 class Synonym {
  private:
