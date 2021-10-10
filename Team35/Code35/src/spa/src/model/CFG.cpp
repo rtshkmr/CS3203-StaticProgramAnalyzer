268dc18badd5f963562579cf77f113cb7d382701
Ritesh Kumar <ritesh@emerald.pink>
2021-10-10 19:02:03 +0800

    Remove unnecessary Cluster fields and add support for Updating start_ and end_
    Existing unit and integration test pass but there's a bug:
    
    BUG:
    * run system test 7, it will throw an error because the body block of If doesn't have it's start_ and end_ updated (still has default -1 for some reason)

--- Team35/Code35/src/spa/src/model/CFG.cpp
+++ Team35/Code35/src/spa/src/model/CFG.cpp
@@ -2,6 +2,7 @@
  * This file contains the implementation required for CFG.
  */
 
+#include <cassert>
 #include "CFG.h"
 
 int Cluster::size() const {
@@ -9,22 +10,16 @@ int Cluster::size() const {
   return end_ - start_ + 1;
 }
 
-void ConditionalBlock::AddControlVariable(VariableName* control_variable) {
-  this->control_variables_.insert(control_variable);
-}
-
-
 /**
  * Adds a new cluster to list of clusters within it.
- * @param new_cluster
+ * @param new_nested_cluster
  */
-void Cluster::AddChildCluster(Cluster* new_cluster) {
-  this->nested_clusters_.push_back(new_cluster);
+void Cluster::AddChildCluster(Cluster* new_nested_cluster) {
+  this->nested_clusters_.push_back(new_nested_cluster);
+  this->UpdateParentClusterRange(new_nested_cluster);
+
 }
 
-bool Cluster::IsVariableModified(VariableName target_variable) {
-  return false;
-}
 void Cluster::AddStmt(StatementNumber statement_number) {
   if (start_ == end_ && start_ == -1) { //new block
     start_ = end_ = statement_number.GetNum();
@@ -69,7 +64,6 @@ void Cluster::SetParentCluster(Cluster* parent_cluster) {
 
 }
 
-
 /**
  * A cluster will be contained within another parent cluster (except if it's the outermost procedure cluster) as an element
  * of a list of nested_clusters. Hence, a sibling cluster refers to a cluster
@@ -80,17 +74,17 @@ Cluster* Cluster::GetNextSiblingCluster() {
   // get the parent cluster if it exists
   Cluster* sibling = nullptr;
   Cluster* parent_cluster = this->GetParentCluster();
-  if(parent_cluster != nullptr) { // i.e. not outmost cluster:
+  if (parent_cluster != nullptr) { // i.e. not outmost cluster:
     std::list<Cluster*> siblings = parent_cluster->GetNestedClusters();
-    std::list<Cluster*>::iterator itr = std::find(siblings.begin(), siblings.end(),this);
-    if(itr!=end(siblings)){ // i.e. this exists, i can find myself using my parent
+    std::list<Cluster*>::iterator itr = std::find(siblings.begin(), siblings.end(), this);
+    if (itr != end(siblings)) { // i.e. this exists, i can find myself using my parent
       int next_sibling_idx = std::distance(siblings.begin(), itr) + 1; // todo: check if 0 or 1 idx
-      if(next_sibling_idx >= siblings.size()) {
+      if (next_sibling_idx >= siblings.size()) {
         // there is no sibling to myself, this is not an error.
         return nullptr;
       } else {
         std::advance(itr, 1);
-        return *itr;
+        return * itr;
       }
     } else {
       // todo: throw exception for this, not possible to have asituation where I'm looking for my next sibling but I can't find myself in my parent's list
@@ -113,10 +107,37 @@ std::list<Cluster*> Cluster::GetNestedClusters() {
 
 void Cluster::AddSiblingCluster(Cluster* new_sibling_cluster) {
   Cluster* existing_parent_cluster = parent_cluster_;
-  if(parent_cluster_ == nullptr) { // the outermost cluster can't have any siblings
+  if (parent_cluster_ == nullptr) { // the outermost cluster can't have any siblings
     throw std::invalid_argument("The outermost cluster (representing a procedure) should never have any siblings");
   }
   parent_cluster_->AddChildCluster(new_sibling_cluster);
+}
+
+/**
+ * When a new_nested_cluster is added to a parent cluster, the parent cluster's range of values for statements, [start_, end_]
+ * needs to be updated with logic similar to AddSmt to reflect the range expanded by the child.
+ * @param new_nested_cluster
+ */
+void Cluster::UpdateParentClusterRange(Cluster* new_nested_cluster) {
+  int new_cluster_start = new_nested_cluster->start_;
+  int new_cluster_end = new_nested_cluster->end_;
+  assert(new_cluster_start <= new_cluster_end
+             && new_cluster_start >= -1 && new_cluster_end >= -1);
+  if(this->start_ == new_cluster_start && this->end_ == new_cluster_end) return;
+  if (this->start_ == end_ && this->start_ == -1) { //new cluster, doesn't have any nested clusters yet
+    this->start_ = new_cluster_start;
+    this->end_ = new_cluster_end;
+  } else { // there are nested clusters within, assume the start and end range already updated
+    bool new_cluster_appears_before_this = new_cluster_end == this->start_ - 1;
+    bool new_cluster_appears_after_this = new_cluster_start == this->end_ + 1;
+    if(new_cluster_appears_before_this){
+      this->start_ = new_cluster_start;
+    } else if (new_cluster_appears_after_this) {
+      this->end_ = new_cluster_end;
+    } else {
+      throw std::invalid_argument("[UpdateParentClusterRange] An input is only valid if statement numbers are continuous with existing ones");
+    }
+  }
 }
 
 // default destructors:
--- Team35/Code35/src/spa/src/model/CFG.h
+++ Team35/Code35/src/spa/src/model/CFG.h
@@ -18,14 +18,12 @@ class Cluster {
  protected:
   int start_ = -1;
   int end_ = -1;
-  std::set<VariableName*>
-      modified_variables_; // QQ: why does it throw error if I use Variable instead of VariableName* !?
   Cluster* parent_cluster_;
-  // todo: is it better to have as a field the position idx in the nested_clusters list of the parent? if not need to do a linear search each time.
  public:
   Cluster() {};
   int size() const;
-  void AddChildCluster(Cluster* new_cluster);
+  void AddChildCluster(Cluster* new_nested_cluster);
+  void UpdateParentClusterRange(Cluster* new_nested_cluster);
   void AddSiblingCluster(Cluster* new_sibling_cluster);
   void AddStmt(StatementNumber statement_number);
   void RemoveStmt(StatementNumber statement_number);
@@ -35,11 +33,7 @@ class Cluster {
   Cluster* GetNextSiblingCluster();
   void SetParentCluster(Cluster* parent_cluster);
   /// ISSUE 2: to use dynamic_cast, need a virtual method; suggesstion -> create a virtual destructor (good practice too)
-  // todo: use Cluster instead of block here
   virtual ~Cluster();
-
-  // todo: add more helper functions:
-  bool IsVariableModified(VariableName target_variable);
 };
 
 class Block : public Cluster {
@@ -66,17 +60,13 @@ class Block : public Cluster {
 class ConditionalBlock : public Block {
  private:
   std::set<VariableName*> control_variables_;
-  std::set<ConstantValue*> control_constants_;
 
  public:
   ~ConditionalBlock();
   ConditionalBlock() {};
-  void AddControlVariable(VariableName* control_variable);
 };
 
 class BodyBlock : public Block {
- private:
-  std::set<VariableName*> modified_variables_;
 
  public:
   ~BodyBlock();
