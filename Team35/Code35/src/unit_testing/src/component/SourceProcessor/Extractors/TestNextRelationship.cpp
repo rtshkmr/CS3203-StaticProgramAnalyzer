f5f31287ecd65ffe9112a9555e2053761197e437
LimJunxue <junxue1@hotmail.com>
2021-10-14 13:03:26 +0800

    Comment out utest for NextExtractor

--- Team35/Code35/src/unit_testing/src/component/SourceProcessor/Extractors/TestNextRelationship.cpp
+++ Team35/Code35/src/unit_testing/src/component/SourceProcessor/Extractors/TestNextRelationship.cpp
@@ -4,30 +4,30 @@
 
 using namespace entity_utils;
 
-TEST_CASE("1.NextExtractor.basic conditions") {
-  Deliverable deliverable;
-
-  SECTION("1 statement") {
-    Procedure* proc1 = GetProc1();
-    Statement* s1 = CreateStatement(GetAssign1(), 1);
-    proc1->AddStatement(s1);
-    proc1->SetBlockRoot();
-    deliverable.AddStatement(s1);
-    NextExtractor next_extractor{};
-    next_extractor.Extract(&deliverable);
-    CHECK(deliverable.next_hash_.count(s1) == 0);
-  }
-
-  SECTION("single level statement list") {}
-  SECTION("1 if") {}
-  SECTION("1 while with next") {}
-  SECTION("1 while without next") {}
-  SECTION("multiple procedures") {}
-}
-
-TEST_CASE("1.NextExtractor.nested containers") {
-  SECTION("if x3") {}
-  SECTION("while x3") {}
-  SECTION("if and while") {}
-  SECTION("multiple nesting of if and while") {}
-}
+//TEST_CASE("1.NextExtractor.basic conditions") {
+//  Deliverable deliverable;
+//
+//  SECTION("1 statement") {
+//    Procedure* proc1 = GetProc1();
+//    Statement* s1 = CreateStatement(GetAssign1(), 1);
+//    proc1->AddStatement(s1);
+//    proc1->SetBlockRoot();
+//    deliverable.AddStatement(s1);
+//    NextExtractor next_extractor{};
+//    next_extractor.Extract(&deliverable);
+//    CHECK(deliverable.next_hash_.count(s1) == 0);
+//  }
+//
+//  SECTION("single level statement list") {}
+//  SECTION("1 if") {}
+//  SECTION("1 while with next") {}
+//  SECTION("1 while without next") {}
+//  SECTION("multiple procedures") {}
+//}
+//
+//TEST_CASE("1.NextExtractor.nested containers") {
+//  SECTION("if x3") {}
+//  SECTION("while x3") {}
+//  SECTION("if and while") {}
+//  SECTION("multiple nesting of if and while") {}
+//}
