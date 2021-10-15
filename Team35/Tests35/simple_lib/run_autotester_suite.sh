34dee2ca571334edd2f43f7c850ee9109358882a
WeiJie <tanweijie123@gmail.com>
2021-10-15 17:30:57 +0800

    Test Parser with 0 and 1 nesting

---
+++ Team35/Tests35/simple_lib/run_autotester_suite.sh
@@ -0,0 +1,24 @@
+# This shell executor will run all the test files in this folder. 
+# How it works? 
+# All test should have a "test_source_x" and "test_queries_x" where x is the same number, and in increasing order
+# Do ensure the test case number are in increasing order. 
+# At the end of the run, it will zip all the .xml file
+# Written by: WeiJie (14/09/2021) 
+
+
+echo "[NOTE] Running parser"; 
+RESULT="0";
+
+for i in $(ls basic/basic_*.txt);
+do echo "[NOTE] Running Test $i"; 
+	./../autotester test_source_$i.txt blank.txt blank.xml > basic_$i.txt;
+	if [ "$?" != 0 ] 
+	then
+	    RESULT="1";
+		echo "[NOTE] $i failed."; 
+	else
+		echo "[NOTE] $i passed."; 
+	fi
+done
+
+exit $RESULT
