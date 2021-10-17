f3f417d788b8f0da51b2afd12b833f5dcf97bcca
Ritesh Kumar <ritesh@emerald.pink>
2021-10-17 09:29:24 +0800

    Add SpaException class skeleton
    Other exceptions to be derived from this class.

--- Team35/Code35/src/spa/src/exception/SyntaxException.h
+++ Team35/Code35/src/spa/src/exception/SyntaxException.h
@@ -8,6 +8,18 @@
 #include <exception>
 #include <stdexcept>
 
+/**
+ * Base class for all SPA exceptions that happen at runtime.
+ */
+ class SpaException: public std::runtime_error {
+  public:
+   SpaException(char const* const message) : std::runtime_error(message) {};
+
+ };
+
+/**
+ * TODO: derive Syntax and Semantic Exception classes from SpaException class
+ */
 class SyntaxException : public std::runtime_error {
  public:
   SyntaxException(char const* const _Message) : std::runtime_error(_Message) {};
