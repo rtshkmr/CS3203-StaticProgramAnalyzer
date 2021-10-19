8bf156c9120d341ac0fea21cd8f397abc9a52442
WeiJie <tanweijie123@gmail.com>
2021-10-19 09:55:02 +0800

    Update all autotester suite to use color

--- Team35/Tests35/simple_lib/basic/run_autotester_suite.sh
+++ Team35/Tests35/simple_lib/basic/run_autotester_suite.sh
@@ -1,9 +1,10 @@
 # This shell executor will run all the test files in this folder. 
 # How it works? 
-# All test should have a "test_source_x" and "test_queries_x" where x is the same number, and in increasing order
-# Do ensure the test case number are in increasing order. 
-# At the end of the run, it will zip all the .xml file
-# Written by: WeiJie (14/09/2021) 
+# Any files within this directory will be treated as the source file and will be matched with the identical-naming file in queries/
+# If the query file is not found, it will be executed as a blank file. 
+# At the end of the run, it will zip all the log and xml file. 
+# Written by: WeiJie (14/09/2021); 
+# Last Updateed: 19/10/2021
 
 #Perform cleanup
 rm output_*.xml; 
@@ -19,11 +20,11 @@ purple="\033[0;35m"
 lightblue="\033[0;36m"
 
 TOTAL=$(ls *.txt | wc -l);
-echo "${purple}Running a grand total of $TOTAL tests...${end}"; 
+echo -e "${purple}Running a grand total of $TOTAL tests...${end}"; 
 RESULT="0";
 
 for i in $(ls *.txt);
-do echo "${lightblue}Running Test $i{$end}"; 
+do echo -e "${lightblue}Running Test $i{$end}"; 
 
 	if [ -f "queries/$i" ]
 	then
--- Team35/Tests35/simple_lib/pql_single/run_autotester_suite.sh
+++ Team35/Tests35/simple_lib/pql_single/run_autotester_suite.sh
@@ -1,21 +1,30 @@
 # This shell executor will run all the test files in this folder. 
 # How it works? 
-# All test should have a "test_source_x" and "test_queries_x" where x is the same number, and in increasing order
-# Do ensure the test case number are in increasing order. 
-# At the end of the run, it will zip all the .xml file
-# Written by: WeiJie (14/09/2021) 
+# Any files within this directory will be treated as the source file and will be matched with the identical-naming file in queries/
+# If the query file is not found, it will be executed as a blank file. 
+# At the end of the run, it will zip all the log and xml file. 
+# Written by: WeiJie (14/09/2021); 
+# Last Updateed: 19/10/2021
 
 #Perform cleanup
 rm output_*.xml; 
 rm log_*.txt;
 rm Team35_SPA*.log;
 
+# Colors
+end="\033[0m"
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+purple="\033[0;35m"
+lightblue="\033[0;36m"
+
 TOTAL=$(ls *.txt | wc -l);
-echo "[NOTE] Running a grand total of $TOTAL tests..."; 
+echo -e "${purple}Running a grand total of $TOTAL tests...${end}"; 
 RESULT="0";
 
 for i in $(ls *.txt);
-do echo "[NOTE] Running Test $i"; 
+do echo -e "${lightblue}Running Test $i{$end}"; 
 
 	if [ -f "queries/$i" ]
 	then
@@ -34,14 +43,21 @@ do echo "[NOTE] Running Test $i";
 		QUERIES=$(grep -o '</query>' $FILENAME | wc -l);
 		PASSES=$(grep -o '<passed/>' $FILENAME | wc -l);
 		FAILS=$(grep -o '</failed>' $FILENAME | wc -l);
-		echo "[NOTE] #Queries = $QUERIES; #Passes = $PASSES; #Fails = $FAILS" 
+		
 		mv Team35_SPA.log Team35_SPA_$i.log # to rename Logger file for each run.
-
-		if [ "$QUERIES" != "$PASSES" ]
+		
+		if [ "$QUERIES" == "0" ]
 		then
-			RESULT="2";
-			echo "[ERROR] $i has failing cases."; 
+			echo -e "${yellow}#Queries = $QUERIES; #Passes = $PASSES; #Fails = $FAILS${end}";
+		else
+			if [ "$QUERIES" != "$PASSES" ]
+			then
+				RESULT="2";
+				echo -e "${red}#Queries = $QUERIES; #Passes = $PASSES; #Fails = $FAILS${end}";
+			else
+				echo -e "${green}#Queries = $QUERIES; #Passes = $PASSES; #Fails = $FAILS${end}";
 		fi
+
 	fi
 done
 
