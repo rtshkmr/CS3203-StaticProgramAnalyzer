c48940972efb6053a74b2e8bb7015c17946b9e77
olivercheok20 <olivercheok20@gmail.com>
2021-10-28 04:37:29 +0800

    Replace ConstantValue with Constant in EntityFactory

--- Team35/Code35/src/spa/src/component/SourceProcessor/EntityFactory.h
+++ Team35/Code35/src/spa/src/component/SourceProcessor/EntityFactory.h
@@ -12,18 +12,18 @@ class EntityFactory {
   EntityFactory() = default;
   EntityFactory(std::list<Procedure*>* proc_list,
                 std::list<Variable*>* var_list,
-                std::list<ConstantValue*>* const_list);
+                std::list<Constant*>* const_list);
   Entity* CreateEntity(std::vector<Token> tokens);
   static std::vector<Token> GetExpressionTokens(std::vector<Token> tokens, TokenTag start_tag, TokenTag end_tag);
  private:
   //attribute
   std::list<Procedure*>* proc_list_;
   std::list<Variable*>* var_list_;
-  std::list<ConstantValue*>* const_list_;
+  std::list<Constant*>* const_list_;
 
   Procedure* CreateProcedure(std::string proc_name);
   Variable* CreateVariable(std::string var_name);
-  ConstantValue* CreateConstantValue(std::string const_val);
+  Constant* CreateConstant(std::string const_val);
   Procedure* RetrieveProcedure(std::string proc_name);
   Variable* RetrieveVariable(std::string var_name);
 
@@ -35,7 +35,7 @@ class EntityFactory {
 
   static std::string ConvertTokensToString(std::vector<Token> tokens);
   std::vector<Variable*> GetVariablesFromExpressionTokens(std::vector<Token> tokens);
-  std::vector<ConstantValue*> GetConstantsFromExpressionTokens(std::vector<Token> tokens);
+  std::vector<Constant*> GetConstantsFromExpressionTokens(std::vector<Token> tokens);
 };
 
 #endif //AUTOTESTER_TEAM00_CODE00_SRC_SPA_SRC_COMPONENT_SOURCEPROCESSOR_ENTITYFACTORY_H_
