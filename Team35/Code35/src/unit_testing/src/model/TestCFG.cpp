1fd38a2b7748fd2a900a51d3ed609e6a4fe16f6a
Ritesh Kumar <ritesh@emerald.pink>
2021-10-30 19:54:15 +0800

    Add unit tests for cluster navigation helpers

--- Team35/Code35/src/unit_testing/src/model/TestCFG.cpp
+++ Team35/Code35/src/unit_testing/src/model/TestCFG.cpp
@@ -54,16 +54,16 @@ TEST_CASE("1.CFG.Block.StatementRange") {
 TEST_CASE("1.CFG.Block.Linkage") {
   SECTION ("Self-Linkage") {
     Block b;
-    b.AddNextBlock(&b); //self-loop block
-    Block* nextBlock = *b.GetNextBlocks().begin();
+    b.AddNextBlock(& b); //self-loop block
+    Block* nextBlock = * b.GetNextBlocks().begin();
 
-    REQUIRE(&b == nextBlock);
+    REQUIRE(& b == nextBlock);
   }
 }
 
 TEST_CASE("1.CFG.Cluster") {
   SECTION("Basic class hierarchies") {
-    SECTION("AddChildClusterToBack and AddSiblingCluster functions"){
+    SECTION("AddChildClusterToBack and AddSiblingCluster functions") {
       Cluster* outer_cluster = new Cluster();
       Cluster* child_cluster = new Cluster();
       Cluster* sibling_cluster = new Cluster();
@@ -75,12 +75,71 @@ TEST_CASE("1.CFG.Cluster") {
       REQUIRE(nested_clusters.size() == 2);
       REQUIRE(nested_clusters.front() == child_cluster);
       REQUIRE(nested_clusters.back() == sibling_cluster);
+      // tests get next and get prev siblings
       Cluster* next_sibling = child_cluster->GetNextSiblingCluster();
       Cluster* prev_sibling = next_sibling->GetPrevSiblingCluster();
       REQUIRE(next_sibling == sibling_cluster);
       REQUIRE(prev_sibling == child_cluster);
-
     }
+  }
+
+  SECTION("Navigation Convenience Helpers") {
+
+    // set up valid if cluster with constituents
+    Cluster* if_cluster = new Cluster();
+    if_cluster->SetClusterTag(ClusterTag::kIfCluster);
+    Cluster* if_cond = new Cluster();
+    Cluster* if_body = new Cluster();
+    Cluster* else_body = new Cluster();
+    if_cond->SetClusterTag(ClusterTag::kIfCond);
+    if_body->SetClusterTag(ClusterTag::kIfBody);
+    else_body->SetClusterTag(ClusterTag::kElseBody);
+    if_cluster->AddChildClusterToBack(if_cond);
+    if_cluster->AddChildClusterToBack(if_body);
+    if_cluster->AddChildClusterToBack(else_body);
+
+
+    // setup valid while cluster with constituents
+    Cluster* while_cluster = new Cluster();
+    Cluster* while_cond = new Cluster();
+    Cluster* while_body = new Cluster();
+    while_cluster->SetClusterTag(ClusterTag::kWhileCluster);
+    while_cond->SetClusterTag(ClusterTag::kWhileCond);
+    while_body->SetClusterTag(ClusterTag::kWhileBody);
+    while_cluster->AddChildClusterToBack(while_cond);
+    while_cluster->AddChildClusterToBack(while_body);
+
+    SECTION("Getting Cluster Constituents") {
+      bool correctly_found_if_cond_constituent = if_cond == if_cluster->GetClusterConstituent(ClusterTag::kIfCond);
+      bool correctly_found_if_body_constituent = if_body == if_cluster->GetClusterConstituent(ClusterTag::kIfBody);
+      bool
+          correctly_found_else_body_constituent = else_body == if_cluster->GetClusterConstituent(ClusterTag::kElseBody);
+      REQUIRE(correctly_found_if_cond_constituent);
+      REQUIRE(correctly_found_if_body_constituent);
+      REQUIRE(correctly_found_else_body_constituent);
+
+      // while cluster with valid constituents:
+      bool correctly_found_while_cond_constituent =
+          while_cond == while_cluster->GetClusterConstituent(ClusterTag::kWhileCond);
+      bool correctly_found_while_body_constituent =
+          while_body == while_cluster->GetClusterConstituent(ClusterTag::kWhileBody);
+      REQUIRE(correctly_found_while_cond_constituent);
+      REQUIRE(correctly_found_while_body_constituent);
+    }
+
+    SECTION("Find next sibling via tags") {
+      bool correctly_found_if_body_from_cond = if_body == if_cond->FindNextSibling(ClusterTag::kIfBody);
+      bool correctly_found_else_body_from_cond = else_body == if_cond->FindNextSibling(ClusterTag::kElseBody);
+      bool correctly_found_else_body_from_if_body = else_body == if_body->FindNextSibling(ClusterTag::kElseBody);
+      REQUIRE(correctly_found_if_body_from_cond);
+      REQUIRE(correctly_found_else_body_from_cond);
+      REQUIRE(correctly_found_else_body_from_if_body);
+    }
+
+  }
+
+  SECTION("Traversal Helper Functions") {
+    //
   }
 }
 
