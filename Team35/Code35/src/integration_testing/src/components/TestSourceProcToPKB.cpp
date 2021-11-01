29b9aedae399fc0a52fcb25cfeb7f6369b586a49
LimJunxue <junxue1@hotmail.com>
2021-11-01 17:47:26 +0800

    Remove extra function to fix merge conflict

--- Team35/Code35/src/integration_testing/src/components/TestSourceProcToPKB.cpp
+++ Team35/Code35/src/integration_testing/src/components/TestSourceProcToPKB.cpp
@@ -5,20 +5,6 @@
 
 using namespace test_util;
 
-bool AreEntityListsEqual(const std::list<std::tuple<EntityEnum, std::string>>& l1, std::vector<Entity*> l2) {
-  for (Entity* a: l2) {
-    bool found_a = false;
-    for (std::tuple<EntityEnum, std::string> b: l1) {
-      bool match = a->GetEntityEnum() == std::get<0>(b) && PKB::GetNameFromEntity(a) == std::get<1>(b);
-      found_a = found_a || match;
-    }
-    if (!found_a) {
-      return false;
-    }
-  }
-  return l2.size() == l1.size();
-}
-
 TEST_CASE("SP to PKB basic retrieval") {
   PKB* pkb = sp::SourceProcessor::ProcessSourceFile("./../../../tests/integration_test_files/basic_source.txt");
   SECTION("Procedure") {
