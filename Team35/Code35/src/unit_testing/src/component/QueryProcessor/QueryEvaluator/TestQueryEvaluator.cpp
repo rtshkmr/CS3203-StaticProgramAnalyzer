19fb30f6c448c4da108019360d511ab03f6438ed
maxxng <maxngkaishen@gmail.com>
2021-11-07 16:48:52 +0800

    Fix unit test and add stress test

--- Team35/Code35/src/unit_testing/src/component/QueryProcessor/QueryEvaluator/TestQueryEvaluator.cpp
+++ Team35/Code35/src/unit_testing/src/component/QueryProcessor/QueryEvaluator/TestQueryEvaluator.cpp
@@ -3,154 +3,118 @@
 #include "component/QueryProcessor/types/Types.h"
 #include "component/PKB/PKB.h"
 #include "../../../../utils/PKBStub.h"
+#include "../../../../utils/EntityUtils.h"
 
-//TEST_CASE("3.QueryProcessor.Query select only") {
-//  Deliverable d1_ = Deliverable();
+static std::vector<int> GetStmtIntValues(std::vector<Entity*> input) {
+  std::vector<int> output;
+  for (auto entity : input) {
+    output.push_back(dynamic_cast<Statement*>(entity)->GetStatementNumber());
+  }
+  return output;
+}
+
+TEST_CASE("3.QueryProcessor.Query select only") {
+  Deliverable* deliverable = SetUpDeliverable_Week4();
+
+  PKB *pkb = new PKB();
+
+  pkb->PopulateDataStructures(*deliverable);
+  auto* dbm = new DBManager(pkb);
+  QueryEvaluator evaluator(dbm);
+  SECTION("Basic select query") {
+    std::vector<Synonym> synonymList = {Synonym("s2", DesignEntity::kStmt),
+                                      Synonym("a1", DesignEntity::kAssign),
+                                      Synonym("call", DesignEntity::kCall)};
+    std::vector<Group*> groupList;
+
+    SECTION("Basic statement select query") {
+      Synonym targetSynonym = Synonym("s2", DesignEntity::kStmt);
+
+      UnformattedQueryResult output = evaluator.EvaluateQuery(groupList);
+      REQUIRE(output.GetBooleanResult());
+      REQUIRE(output.GetTables().empty());
+
+
+//      std::vector<Entity*> entity_list = output.GetTables()[0]->GetResults()[0];
 //
-//  Procedure* p1_ = new Procedure(new ProcedureName("p1"));
-//  Variable* v1_ = new Variable(new VariableName("v1"));
-//  Variable* v2_ = new Variable(new VariableName("v2"));
-//  ConstantValue* const1_ = new ConstantValue("1");
-//  ConstantValue* const2_ = new ConstantValue("2");
-//
-//  AssignEntity* a1_ = new AssignEntity
-//      (v1_,
-//       "v1=1+2",
-//       std::vector<Variable*>{},
-//       std::vector<ConstantValue*>{const1_, const2_}
-//      );
-//  CallEntity* call1_ = new CallEntity(p1_);
-//  PrintEntity* print1_ = new PrintEntity(v1_);
-//  ReadEntity* read1_ = new ReadEntity(v1_);
-//
-//  a1_->SetStatementNumber(new StatementNumber(1));
-//  call1_->SetStatementNumber(new StatementNumber(2));
-//  print1_->SetStatementNumber(new StatementNumber(3));
-//  read1_->SetStatementNumber(new StatementNumber(4));
-//
-//  //  d1_.AddProc(p1_);
-//  //  d1_.AddVariable(v1_);
-//  //  d1_.AddVariable(v2_);
-//  //  d1_.AddConstant(const1_);
-//  //  d1_.AddConstant(const2_);
-//
-//  d1_.var_list_.push_back(v1_);
-//  d1_.var_list_.push_back(v2_);
-//
-//  d1_.AddStatement(a1_);
-//  d1_.AddStatement(call1_);
-//  d1_.AddStatement(print1_);
-//  d1_.AddStatement(read1_);
-//
-//  d1_.AddAssignEntity(a1_);
-//  d1_.AddCallEntity(call1_);
-//  d1_.AddPrintEntity(print1_);
-//  d1_.AddReadEntity(read1_);
-//
-//  d1_.AddFollowRelationship(a1_, call1_);
-//  d1_.AddFollowRelationship(call1_, print1_);
-//  d1_.AddFollowRelationship(print1_, read1_);
-//
-//  PKB pkb = PKB();
-//
-//  pkb.PopulateDataStructures(d1_);
-//
-//  SECTION("Basic select query") {
-//    std::vector<Synonym> synonymList = {Synonym("s2", DesignEntity::kStmt),
-//                                      Synonym("a1", DesignEntity::kAssign),
-//                                      Synonym("call", DesignEntity::kCall)};
-//    std::vector<Group*> groupList;
-//
-//    SECTION("Basic statement select query") {
-//      Synonym targetSynonym = Synonym("s2", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(&pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery(groupList);
-//      std::vector<std::string> answer = {"1", "2", "3", "4"};
-//      REQUIRE(output == answer);
-//    }
-//
-//    SECTION("Basic assign statement select query") {
-//
-//      Synonym targetSynonym = Synonym("a1", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(synonymList, targetSynonym, groupList, pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery();
-//      std::vector<std::string> answer = {"1"};
-//      REQUIRE(output == answer);
-//    }
-//
-//    SECTION("Basic call statement select query") {
-//
-//      Synonym targetSynonym = Synonym("call", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(synonymList, targetSynonym, groupList, pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery();
-//      std::vector<std::string> answer = {"2"};
-//      REQUIRE(output == answer);
-//    }
-//
-//  }
-//
-//  SECTION("Single SuchThat query") {
-//    std::list<Synonym> synonymList = {Synonym("s2", DesignEntity::kStmt),
-//                                      Synonym("a1", DesignEntity::kAssign),
-//                                      Synonym("call", DesignEntity::kCall)};
-//    struct SuchThat st;
-//    st.rel_ref = RelRef::kFollows;
-//
-//    SECTION("With double synonyms") {
-//      st.right_is_synonym = true;
-//      st.left_is_synonym = true;
-//      st.right_hand_side = "call";
-//      st.left_hand_side = "s2";
-//      std::vector<Clause*> clauseList = {& st};
-//      Group firstGroup(clauseList, true);
-//      std::list<Group*> groupList = {& firstGroup};
-//
-//      Synonym targetSynonym = Synonym("s2", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(synonymList, targetSynonym, groupList, pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery();
-//      std::vector<std::string> answer = {"1"};
-//      REQUIRE(output == answer);
-//    }
-//
-//    SECTION("With single Synonym") {
-//      st.right_is_synonym = false;
-//      st.left_is_synonym = true;
-//      st.right_hand_side = "3";
-//      st.left_hand_side = "s2";
-//
-//      std::vector<Clause*> clauseList = {& st};
-//      Group firstGroup(clauseList, true);
-//      std::list<Group*> groupList = {& firstGroup};
-//
-//      Synonym targetSynonym = Synonym("s2", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(synonymList, targetSynonym, groupList, pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery();
-//      std::vector<std::string> answer = {"2"};
-//      REQUIRE(output == answer);
-//    }
-//
-//    SECTION("With no synonyms and both references with correct result") {
-//      st.right_is_synonym = false;
-//      st.left_is_synonym = false;
-//      st.right_hand_side = "3";
-//      st.left_hand_side = "2";
-//
-//      std::vector<Clause*> clauseList = {& st};
-//      Group firstGroup(clauseList, false);
-//      std::list<Group*> groupList = {& firstGroup};
-//
-//      Synonym targetSynonym = Synonym("s2", DesignEntity::kStmt);
-//      QueryEvaluator evaluator(synonymList, targetSynonym, groupList, pkb);
-//
-//      std::vector<std::string> output = evaluator.EvaluateQuery();
-//      std::vector<std::string> answer = {"1", "2", "3", "4"};
-//      REQUIRE(output == answer);
-//    }
+//      std::vector<std::string> actual_answer = GetStmtStringValues(entity_list);
+//      std::vector<std::string> expected_answer = {"1", "2", "3", "4"};
+//      REQUIRE(actual_answer == expected_answer);
+    }
+  }
+
+  SECTION("Single SuchThat query") {
+    Synonym* stmt = new Synonym("s2", DesignEntity::kStmt);
+    Synonym* assign = new Synonym("a1", DesignEntity::kAssign);
+    Synonym* call = new Synonym("call", DesignEntity::kCall);
+    struct SuchThat st;
+    st.rel_ref = RelRef::kFollows;
+
+    SECTION("With double synonyms") {
+      st.left_is_synonym = true;
+      st.right_is_synonym = true;
+      st.left_hand_side = "s2";
+      st.right_hand_side = "a1";
+      st.first_synonym = stmt;
+      st.second_synonym = assign;
+      std::vector<Clause*> clause_list = {& st};
+      std::vector<Synonym *> target_synonym = {stmt};
+      Group firstGroup(clause_list, true, target_synonym);
+      std::vector<Group*> group_list = {& firstGroup};
+
+      UnformattedQueryResult output = evaluator.EvaluateQuery(group_list);
+      bool boolean_output = output.GetBooleanResult();
+      REQUIRE(boolean_output);
+      std::vector<Entity *> entity_list = output.GetTables()[0]->GetResults()[0];
+      std::vector<int> actual_answer = GetStmtIntValues(entity_list);
+      std::vector<int> answer = {2, 3, 4};
+      REQUIRE(actual_answer == answer);
+      REQUIRE(true);
+    }
+
+    SECTION("With single Synonym") {
+      st.left_is_synonym = true;
+      st.right_is_synonym = false;
+      st.left_hand_side = "s2";
+      st.right_hand_side = "3";
+      st.first_synonym = stmt;
+
+      std::vector<Clause*> clause_list = {& st};
+      std::vector<Synonym *> target_synonym = {stmt};
+      Group firstGroup(clause_list, true, target_synonym);
+      std::vector<Group*> group_list = {& firstGroup};
+
+      UnformattedQueryResult output = evaluator.EvaluateQuery(group_list);
+      bool boolean_output = output.GetBooleanResult();
+      REQUIRE(boolean_output);
+      std::vector<Entity *> entity_list = output.GetTables()[0]->GetResults()[0];
+      std::vector<int> actual_answer = GetStmtIntValues(entity_list);
+      std::vector<int> answer = {2};
+      REQUIRE(actual_answer == answer);
+    }
+
+    SECTION("With no synonyms and both references with correct result") {
+      st.right_is_synonym = false;
+      st.left_is_synonym = false;
+      st.right_hand_side = "3";
+      st.left_hand_side = "2";
+
+      std::vector<Clause *> empty_clause_list = {};
+      std::vector<Clause*> clauseList = {& st};
+      std::vector<Synonym *> target_synonym = {stmt};
+      std::vector<Synonym *> empty_target_synonym = {};
+      Group first_group(empty_clause_list, true, target_synonym);
+      Group second_group(clauseList, false, empty_target_synonym);
+      std::vector<Group*> group_list = {& first_group, &second_group};
+
+      UnformattedQueryResult output = evaluator.EvaluateQuery(group_list);
+      bool boolean_output = output.GetBooleanResult();
+      REQUIRE(boolean_output);
+      std::vector<Entity *> entity_list = output.GetTables()[0]->GetResults()[0];
+      std::vector<int> actual_answer = GetStmtIntValues(entity_list);
+      std::vector<int> answer = {1, 2, 3, 4, 5};
+      REQUIRE(actual_answer == answer);
+    }
 //
 //    SECTION("With no synonyms and both references but wrong") {
 //      st.right_is_synonym = false;
@@ -205,9 +169,9 @@
 //      std::vector<std::string> answer = {};
 //      REQUIRE(output == answer);
 //    }
-//  }
-//}
-//
+  }
+}
+
 //TEST_CASE("3.QueryEvaluator.Evaluate single Follow and Parent clauses") {
 //  Deliverable* deliverable = SetUpDeliverable_Week4();
 //  PKB pkb = PKB();
